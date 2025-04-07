f60cd9230dc439d7369ae242e27682959dfaa91a
kkuni-jjang <min212328@naver.com>
2025-04-07 16:19:06 +0900

    PostgreSQL

--- config/production.py
+++ config/production.py
@@ -1,9 +1,14 @@
 from logging.config import dictConfig
 from config.default import *
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'h\xe8\xce\xa4\xea$)ta\x1c\xb2\xc4\x0c?\xf3:'
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 
 dictConfig({
     'version': 1,
