1eae18beb764d122d133a09c34aba0cc07cbf092
kkuni-jjang <min212328@naver.com>
2025-04-07 14:36:43 +0900

    오류 추가

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -12,7 +12,6 @@ def index():
     question_list = Question.query.order_by(Question.create_date.desc())
     return render_template('question/question_list.html', question_list = question_list)
 
-
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     question = Question.query.get(question_id)
@@ -26,6 +25,7 @@ def detail(question_id):
 '''
 @bp.route('/')
 def index():
+    3/0  # 강제로 오류발생
     return redirect(url_for('question._list'))
 '''
 'main'
